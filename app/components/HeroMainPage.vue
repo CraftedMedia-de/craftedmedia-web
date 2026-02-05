95088dbac519c815c7acc911bc21b526e995448b
Letronik <vitaesportsgroup@gmail.com>
2026-02-05 10:30:12 +0100

    Moved Hero section in component

---
+++ app/components/HeroMainPage.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="padding-16"	>
+		<div class="hero hflex-end-between gap-32">
+			<h1 class="type-display-64 text-highlight on-dark">Design und Entwicklung mit dem Nutzer im Fokus</h1>
+			<div class="vflex-start-start gap-32">
+				<p class="type-body-20">CraftedMedia gestaltet nutzerzentrierte Webseiten und Inhalte für Unternehmen, die wachsen – mit klarem Fokus auf Transparenz, hochwertigem Design und einem Nutzererlebnis, das den Menschen konsequent in den Mittelpunkt stellt.</p>
+				<div class="text-highlight on-dark type-heading-20">Für Unternehmen, die nachhaltig wachsen wollen.</div>
+				<div class="hflex-center-start gap-16">
+					<button>Mehr erfahren</button>
+					<button>Projekt unverbindlich besprechen</button>
+				</div>
+			</div>
+			<div class="hero-image"></div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.hero-image {
+	background-color: var(--neutral-900);
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	z-index: -1;
+	border-radius: inherit;
+}
+
+.hero{
+	background: linear-gradient(270deg, rgba(254, 0, 72, 0.00) 50%, rgba(254, 0, 72, 0.20) 100%);
+	border-radius: 1.5rem;
+	height: 95dvh;
+	color: var(--text-body-on-dark);
+	position: relative;
+	padding: 4rem;
+
+	h1 {
+		font-size: 6rem;
+		max-width: 14ch;
+	}
+
+	p {
+		max-width: 45ch;
+	}
+}
+
+button {
+	background-color: red;
+	padding-inline: 2rem;
+	padding-block: 1rem;
+	border-radius: 1rem;
+	color: white;
+	border: none;
+
+	&:hover {
+		background-color: darkred;
+	}
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,48 +1,12 @@
 <script setup lang="ts">
 import NotebookPen from '~/components/icon/NotebookPen.vue';
+import HeroMainPage from '~/components/HeroMainPage.vue';
 
 
 </script>
 
 <template>
-	<div class="padding-16"	>
-		<div class="hero hflex-end-between gap-32">
-			<h1 class="type-display-64 text-highlight on-dark">Design und Entwicklung mit dem Nutzer im Fokus</h1>
-			<div class="vflex-start-start gap-32">
-				<p class="type-body-20">CraftedMedia gestaltet nutzerzentrierte Webseiten und Inhalte für Unternehmen, die wachsen – mit klarem Fokus auf Transparenz, hochwertigem Design und einem Nutzererlebnis, das den Menschen konsequent in den Mittelpunkt stellt.</p>
-				<div class="text-highlight on-dark type-heading-20">Für Unternehmen, die nachhaltig wachsen wollen.</div>
-				<div class="hflex-center-start gap-16">
-					<button>Mehr erfahren</button>
-					<button>Projekt unverbindlich besprechen</button>
-				</div>
-			</div>
-			<div class="hero-image"></div>
-
-		</div>
-	</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	<HeroMainPage />
 
 	<div class="card-grid">
 		<Card
@@ -117,52 +81,11 @@ import NotebookPen from '~/components/icon/NotebookPen.vue';
 </template>
 
 <style scoped>
-.hero-image {
-	background-color: var(--neutral-900);
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	right: 0;
-	left: 0;
-	z-index: -1;
-	border-radius: inherit;
-}
-
-.hero{
-	background: linear-gradient(270deg, rgba(254, 0, 72, 0.00) 50%, rgba(254, 0, 72, 0.20) 100%);
-	border-radius: 1.5rem;
-	height: 95dvh;
-	color: var(--text-body-on-dark);
-	position: relative;
-	padding: 4rem;
-
-	h1 {
-		font-size: 6rem;
-		max-width: 14ch;
-	}
-
-	p {
-		max-width: 45ch;
-	}
-}
 
 .card-grid {
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 	column-gap: 1rem;
 	row-gap: 1rem;
-}
-
-button {
-	background-color: red;
-	padding-inline: 2rem;
-	padding-block: 1rem;
-	border-radius: 1rem;
-	color: white;
-	border: none;
-
-	&:hover {
-		background-color: darkred;
-	}
 }
 </style>
