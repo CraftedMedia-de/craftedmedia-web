df0e34ec9bc704214d286cd1af54932d617677e4
Letronik <vitaesportsgroup@gmail.com>
2026-02-05 11:38:47 +0100

    updated strong to new logic

--- app/components/section/HeroMainPage.vue
+++ app/components/section/HeroMainPage.vue
@@ -1,14 +1,23 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<div class="padding-16"	>
+	<div class="padding-16">
 		<div class="hero hflex-end-between gap-32">
-			<h1 class="type-display-64 text-highlight on-dark">Design und Entwicklung mit dem Nutzer im Fokus</h1>
+			<h1 class="type-display-64 text-highlight on-dark">
+				Design und Entwicklung mit dem Nutzer im Fokus
+			</h1>
 			<div class="vflex-start-start gap-32">
-				<p class="type-body-20"><strong>CraftedMedia</strong> gestaltet nutzerzentrierte Webseiten und Inhalte für Unternehmen, die wachsen – mit klarem Fokus auf Transparenz, hochwertigem Design und einem Nutzererlebnis, das den Menschen konsequent in den Mittelpunkt stellt.</p>
-				<div class="text-highlight on-dark type-heading-20">Für Unternehmen, die nachhaltig wachsen wollen.</div>
+				<p class="type-body-20">
+					<strong class="on-dark">CraftedMedia</strong>
+					gestaltet nutzerzentrierte Webseiten und Inhalte für
+					Unternehmen, die wachsen – mit klarem Fokus auf
+					Transparenz, hochwertigem Design und einem
+					Nutzererlebnis, das den Menschen konsequent in den
+					Mittelpunkt stellt.
+				</p>
+				<div class="text-highlight on-dark type-heading-20">
+					Für Unternehmen, die nachhaltig wachsen wollen.
+				</div>
 				<div class="hflex-center-start gap-16">
 					<button>Mehr erfahren</button>
 					<button>Projekt unverbindlich besprechen</button>
@@ -31,8 +40,12 @@
 	border-radius: inherit;
 }
 
-.hero{
-	background: linear-gradient(270deg, rgba(254, 0, 72, 0.00) 50%, rgba(254, 0, 72, 0.20) 100%);
+.hero {
+	background: linear-gradient(
+		270deg,
+		rgba(254, 0, 72, 0) 50%,
+		rgba(254, 0, 72, 0.2) 100%
+	);
 	border-radius: 1.5rem;
 	height: 95dvh;
 	color: var(--text-body-on-dark);
