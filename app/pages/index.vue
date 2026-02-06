c664e0d78fa0b3189a4d177c2e743dbda0d9c2fa
Letronik <vitaesportsgroup@gmail.com>
2026-02-06 12:04:26 +0100

    removed old code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import NotebookPen from '~/components/icon/NotebookPen.vue';
 import HeroMainPage from '~/components/section/HeroMainPage.vue';
 import MediaBlock from '~/components/section/MediaBlock.vue';
 import Services from '~/components/section/Services.vue';
@@ -28,11 +27,4 @@ import CustomersAndPartners from '~/components/section/CustomersAndPartners.vue'
 	</section>
 </template>
 
-<style scoped>
-.card-grid {
-	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-	column-gap: 1rem;
-	row-gap: 1rem;
-}
-</style>
+<style scoped></style>
