481cd0da096775d7c7035923eb36ae7665ccc6fa
Letronik <vitaesportsgroup@gmail.com>
2026-02-06 11:39:57 +0100

    added 2 new icon components

---
+++ app/components/icon/CodeXML.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<svg
+		class="icon-svg"
+		viewBox="0 0 32 32"
+		aria-hidden="true"
+		fill="none">
+		<path
+			d="M23.9974 21.333L29.3307 15.9997L23.9974 10.6663M7.9974 10.6663L2.66406 15.9997L7.9974 21.333M19.3307 5.33301L12.6641 26.6663"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</svg>
+</template>
+
+<style scoped></style>
