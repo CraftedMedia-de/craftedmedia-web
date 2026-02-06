2c72943ff30545bdb62d6ac1e5b985cdde329ea0
Letronik <vitaesportsgroup@gmail.com>
2026-02-06 12:01:02 +0100

    updated CustomerShowcase.vue with a logo wrapper

--- app/components/CustomerShowcase.vue
+++ app/components/CustomerShowcase.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
 interface Props {
-	paragraph: string
-
+	paragraph: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
 	paragraph: 'Inhalt fehlt',
-
-})
+});
 </script>
 
 <template>
 	<div class="vflex-start-start gap-16">
-		<slot />
+		<div class="logo-wrapper">
+			<slot />
+		</div>
 		<p class="type-body-14">{{ props.paragraph }}</p>
 	</div>
 </template>
 
 <style scoped>
-
+.logo-wrapper {
+	max-height: 2rem;
+}
 </style>
