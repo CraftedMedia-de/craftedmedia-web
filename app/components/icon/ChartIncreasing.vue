7f7c9673c098f7025b698c73c06e0918f610427d
Letronik <vitaesportsgroup@gmail.com>
2026-02-18 13:22:02 +0100

    added new icons

---
+++ app/components/icon/ChartIncreasing.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<svg
+		class="icon-svg"
+		viewBox="0 0 20 20"
+		aria-hidden="true"
+		fill="none">
+		<path
+			d="M4.16406 17.5V12.5M9.9974 17.5V7.5M15.8307 17.5V2.5"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</svg>
+</template>
+
+<style scoped></style>
