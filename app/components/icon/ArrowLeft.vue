480423ede035fee61f0a465cd44063475a385f45
Letronik <vitaesportsgroup@gmail.com>
2026-04-28 14:22:40 +0200

    added new icon

---
+++ app/components/icon/ArrowLeft.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<svg
+		class="icon-svg"
+		viewBox="0 0 24 24"
+		aria-hidden="true"
+		fill="none">
+		<path
+			d="M14 18L8 12L14 6"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</svg>
+</template>
+
+<style scoped></style>
