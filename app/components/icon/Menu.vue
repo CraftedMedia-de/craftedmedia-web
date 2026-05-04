9cc46217bae191770fad577d14aba1ed4637c434
Letronik <vitaesportsgroup@gmail.com>
2026-05-04 14:15:11 +0200

    added new icons

---
+++ app/components/icon/Menu.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<svg
+		class="icon-svg"
+		viewBox="0 0 24 24"
+		aria-hidden="true"
+		fill="none">
+		<path
+			d="M4 5H20M4 12H20M4 19H20"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</svg>
+</template>
+
+<style scoped></style>
