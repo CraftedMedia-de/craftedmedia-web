45c0486e77a6823834f3add4f99f342dc68eb2d3
Letronik <vitaesportsgroup@gmail.com>
2026-05-06 13:49:16 +0200

    added 2 new icons

---
+++ app/components/icon/Panels-Top-Left.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<svg
+		class="icon-svg"
+		viewBox="0 0 32 32"
+		aria-hidden="true"
+		fill="none">
+		<path
+			d="M4 12H28M12 28V12M6.66667 4H25.3333C26.8061 4 28 5.19391 28 6.66667V25.3333C28 26.8061 26.8061 28 25.3333 28H6.66667C5.19391 28 4 26.8061 4 25.3333V6.66667C4 5.19391 5.19391 4 6.66667 4Z"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</svg>
+</template>
+
+<style scoped></style>
